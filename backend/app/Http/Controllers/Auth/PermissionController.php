1eb0ec15f0e074a4cfd536371801630186586f6e
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2025-05-10 22:31:39 +0200

    ft: add permission and role add controller methods

--- backend/app/Http/Controllers/Auth/PermissionController.php
+++ backend/app/Http/Controllers/Auth/PermissionController.php
@@ -4,7 +4,11 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use Symfony\Component\HttpFoundation\Response as ResponseAlias;
 
 class PermissionController extends Controller
 {
@@ -12,4 +16,27 @@ class PermissionController extends Controller
         $permissions = Permission::all();
         return response()->json($permissions);
     }
+
+    public function store(Request $request): JsonResponse {
+        $this->validate($request, [
+            'name' => 'required|unique:permissions,name'
+        ]);
+
+        $permission = Permission::create(['name' => $request->input('name')]);
+        return response()->json($permission);
+    }
+
+    public function destroy(Request $request, Permission $permission): JsonResponse {
+        $roles = Role::all();
+        foreach ($roles as $role) {
+            $role->revokePermissionTo($permission);
+        }
+
+        $permission->delete();
+
+        return response()->json([
+            'message' => 'Permission deleted and revoked from all roles.'
+        ], ResponseAlias::HTTP_NO_CONTENT);
+    }
+
 }
--- backend/app/Http/Controllers/Auth/RoleController.php
+++ backend/app/Http/Controllers/Auth/RoleController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
 use Spatie\Permission\Models\Role;
 
 class RoleController extends Controller
@@ -13,4 +14,20 @@ class RoleController extends Controller
     {
         return Role::all();
     }
+
+    public function store(Request $request):JsonResource {
+        $this->validate($request, [
+            'name' => 'required|unique:roles,name',
+            'permission' => 'required',
+        ]);
+
+        $permissionsID = array_map('intval', $request->input('permission'));
+
+        $role = Role::create(['name' => $request->input('name')]);
+        $role->syncPermissions($permissionsID);
+
+        return  $role->toResource();
+
+    }
+
 }
