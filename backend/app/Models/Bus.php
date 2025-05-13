d54df65b30928c5c3975d0f94f56bea8979837a4
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2025-05-13 23:19:27 +0200

    refactor: add a way to dynmically generate bus seats

--- backend/app/Models/Bus.php
+++ backend/app/Models/Bus.php
@@ -7,5 +7,34 @@ use Illuminate\Database\Eloquent\Model;
 class Bus extends Model
 {
     protected $fillable = ['plate_number', 'status'];
-    //
+
+    public function seats()
+    {
+        return $this->hasMany(Seat::class);
+    }
+
+    protected static function booted()
+    {
+        static::created(function ($bus) {
+            $seats = [];
+            $columns = [
+                'A' => 12,
+                'B' => 12,
+                'C' => 1,
+                'D' => 12,
+                'E' => 13,
+            ];
+            foreach ($columns as $column => $count) {
+                for ($i = 1; $i <= $count; $i++) {
+                    $seats[] = [
+                        'seat_number' => $column . $i,
+                        'bus_id' => $bus->id,
+                        'created_at' => now(),
+                        'updated_at' => now(),
+                    ];
+                }
+            }
+            Seat::insert($seats);
+        });
+    }
 }
