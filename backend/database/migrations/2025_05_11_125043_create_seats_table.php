84057ed81d243335457ea67651b2bdffc8af9cb2
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2025-05-13 23:17:32 +0200

    db: add seats related migrations

---
+++ backend/database/migrations/2025_05_11_125043_create_seats_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('seats', function (Blueprint $table) {
+            $table->id();
+            $table->string('seat_number', 3); // e.g., 'A1', 'B12', 'E13'
+            $table->foreignId('bus_id')->constrained()->onDelete('cascade');
+            $table->unique(['bus_id', 'seat_number']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('seats');
+    }
+};
