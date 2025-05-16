53f9dc971a368d18309834b4fe8a934b6fcdbf9b
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2025-05-16 17:25:33 +0200

    add database seeding to entrypoint.sh to populate database after migrations

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,5 +3,8 @@
 # Run Laravel migrations
 php /var/www/html/artisan migrate --force
 
+# Run Laravel database seeding
+php /var/www/html/artisan db:seed --force
+
 # Start Apache
 exec apache2-foreground
